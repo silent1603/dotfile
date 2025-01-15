f1d8269422393390402b5575c8a772e6fd47b2f7
Dao Quoc Trung <trung.daoquoc@gameloft.com>
2025-01-15 10:54:05 +0700

    Create init.el

---
+++ emacs/init.el
@@ -0,0 +1,88 @@
+(setq ring-bell-function 'ignore)
+
+(setq package-archives
+	  '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
+		("MELPA"        . "https://melpa.org/packages/")
+		("ORG"          . "https://orgmode.org/elpa/")
+		("MELPA Stable" . "https://stable.melpa.org/packages/")
+		("nongnu"       . "https://elpa.nongnu.org/nongnu/"))
+	  package-archive-priorities
+	  '(("GNU ELPA"     . 20)
+		("MELPA"        . 15)
+		("ORG"          . 10)
+		("MELPA Stable" . 5)
+		("nongnu"       . 0)))
+
+(package-initialize)
+;; Go ahead and refresh your package list to
+;; make sure everything is up-to-date
+(unless package-archive-contents
+  (package-refresh-contents))
+(blink-cursor-mode 0)   ;; Solid cursor, not blinking
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(when (member "Roboto Mono" (font-family-list))
+  (set-face-attribute 'default nil :font "Roboto Mono" :height 108)
+  (set-face-attribute 'fixed-pitch nil :family "Roboto Mono"))
+
+(when (member "Source Sans Pro" (font-family-list))
+  (set-face-attribute 'variable-pitch nil :family "Source Sans Pro" :height 1.18))
+
+(setq inhibit-startup-message t)  ; Comment at end of line!
+;; Display line numbers in every buffer
+(global-display-line-numbers-mode 1)
+
+(require 'org)
+
+(delete-other-windows)
+(split-window-horizontally)
+
+(setq gc-cons-threshold 100000000) ; 100 mb
+(setq read-process-output-max (* 1024 1024)) ; 1mb
+(global-auto-revert-mode t)
+(setq ring-bell-function 'ignore)
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-gruvbox t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (nerd-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil doom-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(setq evil-normal-state-cursor '(box "#18c936")
+      evil-insert-state-cursor '(bar "medium sea green")
+      evil-visual-state-cursor '(hollow "orange"))
